a15730d1e54d0de8ec9eb1f5c1aa24ec310ee127
Soraya Reyhan <soraya.reyhan@gmail.com>
2022-06-06 19:44:19 +0200

    print param.data

--- src/trainer/online_explicit.py
+++ src/trainer/online_explicit.py
@@ -43,9 +43,9 @@ def explicit_step(
     net: nn.Module, net_prev: nn.Module, imp: Dict[str, torch.Tensor], prev_imp: Dict[str, torch.Tensor],
 ):
     net_prev_params = net_prev.state_dict()
-    avg_alp=0
-    sum_alp=0
+    list_alp=[]
     i=0
+    
     for name, param in net.named_parameters():
         i=i+1
         if param.grad is not None:
@@ -54,10 +54,11 @@ def explicit_step(
             alp_prev = prev_imp[name] ** (1 / 2)  # alpha previous
             alp = alp_new / (alp_new + alp_prev + 1e-20)  # R_j
             param.data = alp * param.data + (1 - alp) * prev_param.data  # interpolation
-            print(torch.mean(alp),'alp')
-            #sum_alp=alp+sum_alp
-    #avg_alp=sum_alp/i
-    #print(avg_alp)
+            #print(torch.mean(alp),'alp')
+            list_alp.append(torch.mean(alp))
+            print(list_alp)
+
+
 
             
 class OnlineExplicitTrainer:
