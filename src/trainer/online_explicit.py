9255bf4b4feeaa628ca1ca83132a061d0ecd0201
Soraya Reyhan <soraya.reyhan@gmail.com>
2022-06-06 19:54:15 +0200

    print param.data

--- src/trainer/online_explicit.py
+++ src/trainer/online_explicit.py
@@ -56,7 +56,8 @@ def explicit_step(
             param.data = alp * param.data + (1 - alp) * prev_param.data  # interpolation
             #print(torch.mean(alp),'alp')
             list_alp.append(torch.mean(alp))
-            print(torch.mean(torch.stack(list_alp)))
+            mean_list_alp=torch.mean(torch.stack(list_alp))
+            print(mean_list_alp)
 
 
 
