264fe0c7a1fa4b772edb7846a934a3efc92cf5c6
Soraya Reyhan <soraya.reyhan@gmail.com>
2022-06-08 18:35:14 +0200

    replay

--- train.py
+++ train.py
@@ -116,7 +116,7 @@ criterion = nn.CrossEntropyLoss()
 #%%
 # train
 num_tasks=5
-buffer_size=30
+buffer_size=300
 train_dataloaders_0_1, train_dataloaders_0_2, train_dataloaders_0_3, train_dataloaders_0_4 =[], [], [], []
 train_dataloaders_1_2, train_dataloaders_1_3, train_dataloaders_1_4=[], [], []
 train_dataloaders_2_3, train_dataloaders_2_4=[], []
