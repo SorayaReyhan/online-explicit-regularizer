d1c4754e25cddbc6f50398349c33e9730ac7ce5d
Soraya Reyhan <soraya.reyhan@gmail.com>
2022-06-08 23:23:06 +0200

    replay

--- train.py
+++ train.py
@@ -180,12 +180,10 @@ for i in range(int(buffer_size/4)):
 
 for task in range(num_tasks):
     if task==1:
-        # train_dev_sets = torch.utils.data.ConcatDataset([train_dataloaders[task], train_dataloaders_0_1])
-        # train_dev_loader = DataLoader(dataset=train_dev_sets)
-        # train_dataloaders[task]=train_dev_loader
-        train_dataloaders[task].append(train_dataloaders_0_1)
+        train_dev_sets = torch.utils.data.ConcatDataset([train_dataloaders[task], train_dataloaders_0_1])
+        train_dev_loader = DataLoader(dataset=train_dev_sets)
+        train_dataloaders[task]=train_dev_loader
 
-        
     elif task==2:
         train_dev_sets = torch.utils.data.ConcatDataset([train_dataloaders[task], train_dataloaders_0_2])
         train_dev_sets = torch.utils.data.ConcatDataset([train_dev_sets, train_dataloaders_1_2])
